3fde71c89ced21b2994c34e5a5a6313d74bfcaf9
Sora <22500168+noobsora@users.noreply.github.com>
2025-07-30 05:11:45 +0800

    Update generate_adlist.py

--- generate_adlist.py
+++ generate_adlist.py
@@ -40,10 +40,10 @@ SOURCES = {
     #    ),
     #    "1Hosts-Lite": ("https://badmojr.github.io/1Hosts/Lite/domains.txt"),
     #    "OISD-Blocklist-Small": ("https://small.oisd.nl/domainswild2"),
-    #    "AWAvenue-Ads-Rule": (
-    #        "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/"
-    #        "main/Filters/AWAvenue-Ads-Rule-RouterOS-Adlist.txt"
-    #    ),
+    "AWAvenue-Ads-Rule": (
+        "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/"
+        "main/Filters/AWAvenue-Ads-Rule-RouterOS-Adlist.txt"
+    ),
     #    "217heidai-AdblockHostsLite": (
     #        "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/"
     #        "adblockhostslite.txt"
